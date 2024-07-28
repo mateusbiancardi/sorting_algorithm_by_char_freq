c4e3ca4a69d9801a17aab546a38bb2d5fada77c8
Mateus Biancardi <mateusbbdasilva@gmail.com>
2024-07-28 18:40:18 -0300

    remove: lista nao utilizada

--- ordenacao.py
+++ ordenacao.py
@@ -1,5 +1,4 @@
 dicio = {}
-listaPalavra = []
 listaValor = []
 
 palavras = input("Digite as palavras separadas por vírgula e espaço: ")
@@ -13,18 +12,17 @@ for palavra in palavras:
   dicio.update({palavra: qntd})
 
 # Pega as infos do dicionário e coloca em dois arrays
-for palavra, valor in dicio.items():
-  listaPalavra.append(palavra)
+for _, valor in dicio.items():
   listaValor.append(valor)
 
 listaValor.sort(reverse=True) # .sort não retorna o valor da lista, ele modifica o valor original
-listaPalavraSorted = []
+listaPalavra = []
 
 # Compara o valor sorted com os valores do dicionário. Se o valor não existe na listaPalavraSorted, adiciona ao final do array.
 # Como o for vai percorrer na ordem decrescente (ou ordem de maior quantidade de letras), então ele adiciona a palavra correspondente ao valor sem repetição de palavra.
 for valorSorted in listaValor:
   for palavra, valor in dicio.items():
-    if valor == valorSorted and palavra not in listaPalavraSorted:
-      listaPalavraSorted.append(palavra)
+    if valor == valorSorted and palavra not in listaPalavra:
+      listaPalavra.append(palavra)
 
-print (listaPalavraSorted)
+print (listaPalavra)
